bc8fa06b6da49eb0cc28b5df33d96b2e8539aa14
Lee Kwenwon <highfence@naver.com>
2018-01-08 23:55:59 +0900

    [ADD] LaunchSceneManager 주석 추가

--- NextManComing_Unity/Assets/Scripts/Scene/0. LaunchScene/LaunchSceneManager.cs
+++ NextManComing_Unity/Assets/Scripts/Scene/0. LaunchScene/LaunchSceneManager.cs
@@ -3,13 +3,19 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// 게임 시작 전에 처리해야할 것들을 처리하는 클래스.
-
+/// <summary>
+/// 게임 시작전 처리가 필요한 작업을 미리 진행하는 클래스.
+/// 스크린, 마우스 설정. 리소스 로드. 모든 싱글톤 클래스의 인스턴스화를 담당한다.
+/// 추가적으로 로딩 씬을 재생한다.
+/// </summary>
 public class LaunchSceneManager : MonoBehaviour
 {
+	/// <summary>
+	/// 모든 초기화 작업을 시작한다.
+	/// TODO :: 클라이언트 설정을 Config로 읽어와야 한다.
+	/// </summary>
 	private void Start()
 	{
-		// TODO :: 클라이언트 Config 읽어오기.
 		ScreenInitialize(1280, 800, false);
 
 		MouseInitialize();
@@ -21,17 +27,28 @@ public class LaunchSceneManager : MonoBehaviour
 		NetworkManager.GetInstance();
 	}
 
+	/// <summary>
+	/// 마우스 설정 초기화 메서드.
+	/// </summary>
 	private static void MouseInitialize()
 	{
 		Cursor.lockState = CursorLockMode.Confined;
 	}
 
+	/// <summary>
+	/// 스크린 설정 초기화 메서드.
+	/// </summary>
+	/// <param name="width"></param>
+	/// <param name="height"></param>
+	/// <param name="isFullScreen"></param>
 	private static void ScreenInitialize(int width, int height, bool isFullScreen)
 	{
 		Screen.SetResolution(width, height, isFullScreen);
 	}
 
-	// 초기화가 진행되는 동안 시간을 벌며 시작 스프라이트를 띄워주는 메소드.
+	/// <summary>
+	/// 초기화가 진행되는 동안 시간을 벌며 시작 로딩씬을 진행해주는 메서드.
+	/// </summary>
 	IEnumerator OnClassLoad()
 	{
 		var loadingRenderer = GetComponent<SpriteRenderer>();
--- NextManComing_Unity/Assets/Scripts/Scene/3. Lobby/ChatService.cs
+++ NextManComing_Unity/Assets/Scripts/Scene/3. Lobby/ChatService.cs
@@ -2,7 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 로비씬 플레이어의 채팅을 가능하게 하는 컴포넌트.
+/// </summary>
 public class ChatService : MonoBehaviour
 {
+	[SerializeField]
+	public GameObject MessageBox;
+
+	[SerializeField]
+	public GameObject Message;
+
+	private NetworkManager network;
+
+	private void Start()
+	{
+		network = NetworkManager.GetInstance();
+	}
+
 
 }
