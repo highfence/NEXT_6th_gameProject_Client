7369dfd38ccf2b66afc3f2e87a5776d2702043f5
Lee Kwenwon <highfence@naver.com>
2018-01-09 00:18:54 +0900

    [ADD] MessageBox 작업중

--- NextManComing_Unity/Assets/Scripts/Scene/3. Lobby/ChatService.cs
+++ NextManComing_Unity/Assets/Scripts/Scene/3. Lobby/ChatService.cs
@@ -13,12 +13,48 @@ public class ChatService : MonoBehaviour
 	[SerializeField]
 	public GameObject Message;
 
-	private NetworkManager network;
+	[SerializeField]
+	public bool IsValiable = false;
+
+	private NetworkManager network = null;
+
 
 	private void Start()
 	{
 		network = NetworkManager.GetInstance();
+
+		if (network == null || UIInitialize() != false)
+		{
+			IsValiable = true;	
+		}
 	}
 
+	/// <summary>
+	/// 사용할 UI 요소들을 초기화해주는 메서드.
+	/// </summary>
+	/// <returns></returns>
+	private bool UIInitialize()
+	{
+		
+
+		return true;
+	}
+
+	/// <summary>
+	/// 메시지를 설정하고 이를 UI에 띄워준다. 
+	/// 내부에서는 우선 메시지를 받아 패킷을 보내주고, 이에 해당하는 응답이 올 경우에
+	/// 메시지를 띄운다. (따라서 다소의 레이턴시가 발생할 수 있다.)
+	/// </summary>
+	/// <param name="msg"></param>
+	/// <returns></returns>
+	public bool SetMessage(string msg)
+	{
+		if (IsValiable == false)
+		{
+			return false;
+		}
+
+		return true;
+	}
 
 }
