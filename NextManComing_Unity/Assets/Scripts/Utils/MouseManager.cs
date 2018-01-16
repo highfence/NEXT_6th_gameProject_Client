b4b6d4b73778a861394620d3029324666a67f048
Lee Kwenwon <highfence@naver.com>
2018-01-17 00:40:16 +0900

    [REFACTORING] 마우스 매니저 관련 코드 정리

--- NextManComing_Unity/Assets/Scripts/Utils/MouseManager.cs
+++ NextManComing_Unity/Assets/Scripts/Utils/MouseManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[AddComponentMenu("RotatePlayer/Mouse Look")]
 public class MouseManager : MonoBehaviour
 {
 	public bool IsEnterPressed = false;
@@ -31,13 +30,7 @@ public class MouseManager : MonoBehaviour
 
 		if (IsEnterPressed == false)
 		{
-			RotateOnBoundary();
-
 		}
-
-		// FOR DEBUG
-		mouseX = Input.mousePosition.x;
-		mouseY = Input.mousePosition.y;
 	}
 
 	// TODO :: 이렇게 하고 보니 엔터키의 상태를 일정하게
@@ -56,29 +49,6 @@ public class MouseManager : MonoBehaviour
 				IsEnterPressed = true;
 				Cursor.visible = true;
 			}
-		}
-	}
-
-
-	// 마우스가 화면의 가장자리에 있다고 판단되었을 때
-	// 계속 화면을 돌려주는 함수.
-	private void RotateOnBoundary()
-	{
-		// 오른쪽으로 돌기.
-		if (Input.mousePosition.x > screenWidth - boundary)
-		{
-
-		}
-		else
-		{
-		}
-		// 왼쪽으로 돌기.
-		if (Input.mousePosition.x < 0 + boundary)
-		{
-		}
-		else
-		{
-
 		}
 	}
 }
