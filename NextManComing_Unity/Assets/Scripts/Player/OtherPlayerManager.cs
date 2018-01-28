9abef561c2b6a08bd1bea734d2cd6a52ac2c7649
Lee Kwenwon <highfence@naver.com>
2018-01-28 16:46:06 +0900

    [ADD] Add OTHER PLAYER Prefab path

--- NextManComing_Unity/Assets/Scripts/Player/OtherPlayerManager.cs
+++ NextManComing_Unity/Assets/Scripts/Player/OtherPlayerManager.cs
@@ -20,7 +20,7 @@ public class OtherPlayerManager : MonoBehaviour
 		playerPool = new GameObject[poolSize];
 
 		// TODO :: 프리팹 지정해줘야함.
-		var playerPrefab = Resources.Load("");
+		var playerPrefab = Resources.Load("TestNpc");
 		
 		for (var i = 0; i < poolSize; ++i)
 		{
